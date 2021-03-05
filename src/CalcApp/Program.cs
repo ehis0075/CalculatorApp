79209fdc4d1276ffa49e10a4cca489e7d63c45bb
Ehis <ehisjude420@gmail.com>
2021-03-05 16:19:35 +0100

    added a while loop

--- src/CalcApp/Program.cs
+++ src/CalcApp/Program.cs
@@ -8,9 +8,13 @@ namespace CalcApp
         static void Main(string[] args)
         {
 
-        
+        int a, b, c;
+        ConsoleKeyInfo status;
 
-        Console.WriteLine("Welcome to my Calculator Application");
+         while(true)
+         {
+             
+            Console.WriteLine("Welcome to my Calculator Application");
             Console.WriteLine("---------------------------------");
             Console.WriteLine("Press any following key to perform an arithmetric operation:");
             Console.WriteLine("---------------------------------");
@@ -22,38 +26,30 @@ namespace CalcApp
             Console.Write("Enter Choice(1-4):");
             int answer = Int32.Parse(Console.ReadLine());
 
-            int a, b, c;
+            Console.Write("Enter the first number:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number:");
+            b = Convert.ToInt32(Console.ReadLine());
+         
             switch(answer)
             {
                 case 1:
-                    Console.Write("Enter the first number:");
-                    a = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Enter the second number:");
-                    b = Convert.ToInt32(Console.ReadLine());
+                    
                     c = a + b;
                     Console.WriteLine("The Sum of the two numbers is = {0}", c);
                     break;
                 case 2:
-                    Console.Write("Enter the first number:");
-                    a = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Enter the second number:");
-                    b = Convert.ToInt32(Console.ReadLine());
+                   
                     c = a - b;
                     Console.WriteLine("The Difference between the two numbers is = {0}", c);
                     break;
                 case 3:
-                    Console.Write("Enter the first number:");
-                    a = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Enter the second number:");
-                    b = Convert.ToInt32(Console.ReadLine());
+                    
                     c = a * b;
                     Console.WriteLine("The Product of the two numbers is = {0}", c);
                     break;
                 case 4:
-                    Console.Write("Enter the first number:");
-                    a = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Enter the second number:");
-                    b = Convert.ToInt32(Console.ReadLine());
+                   
                     c = a / b;
                     Console.WriteLine("The Quotient of the two numbers is = {0}", c);
                     break;
@@ -62,7 +58,15 @@ namespace CalcApp
                     break;
             }
            
-            Console.ReadKey();
+            Console.WriteLine("\nDo you want to continue again (Y/N)?");
+            status = Console.ReadKey();
+            if (status.Key == ConsoleKey.Y)
+            {
+                break;
+            }
+            Console.Clear();
+
+         }
 
         }
     }
